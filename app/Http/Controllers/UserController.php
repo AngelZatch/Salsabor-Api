533c0a5715a9b6251fd45b9e6fb8e0de5be9caaa
AngelZatch <pinbouen.andreas@gmail.com>
2017-06-29 07:34:25 +0200

    init user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	/*public function __construct()
+	{
+		$this->middleware('auth');
+	}*/
+
+    // GET a user profile
+    public function show($id)
+    {
+
+    }
+}
